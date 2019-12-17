30293a3031c8bc577a5820ede6fa8deaf86adc8c
강슬기 <skkang@tmon.co.kr>
2019-12-17 16:04:19 +0900

    Complete '모의고사'

--- src/main/scala/Level_1/skkang/Solution_01.scala
+++ src/main/scala/Level_1/skkang/Solution_01.scala
@@ -1,41 +1,66 @@
 package Level_1.skkang
 
+import scala.collection.immutable.List
+import scala.collection.mutable.MutableList
+
 //모의고사
 object Solution_01 {
 
-//  def main(args: Array[String]): Unit = {
-//    val inputAnswers = scala.io.StdIn.readLine()
-//    val input = inputAnswers.substring(1, inputAnswers.length()-1)
-//    val inputArray = input.split(",")
-//
-//    //가변길이 입력값때문에 arrayBuffer 선택
-//    var inputArrayBuffer = scala.collection.mutable.ArrayBuffer(inputArray: _*)
-//
-//    for(temp <- inputArray){
-//      inputArrayBuffer :+ temp.toInt
-//    }
-//    var a: String = inputArrayBuffer(0)
-//    var b: Int = inputArrayBuffer(0).toInt
-//    println(">> inputArrayBuffer: ",inputArrayBuffer)
-//    println(">> inputArrayBuffer: ",inputArrayBuffer(0))
-//    println(">> inputArrayBuffer: ",inputArrayBuffer(0).toInt)
-//    //1. 입력 값 input 을 Vector 로 변경
-//    //    solution(inputArrayBuffer.toVector)
-//  }
-
   def main(args: Array[String]): Unit = {
-    val answers = scala.io.StdIn.readLine()
+    val inputAnswers = scala.io.StdIn.readLine()
+    val input = inputAnswers.substring(1, inputAnswers.length()-1)
+    val inputArray = input.split(",")
+
+    //가변길이 입력값때문에 arrayBuffer 선택
+//    var inputArrayBuffer = scala.collection.mutable.ArrayBuffer(inputArray: _*)
+    var inputMutableArray = new Array[Int](inputArray.length)
+    var i = 0
+
+    inputArray.foreach{ x =>
+      inputMutableArray(i) = x.toInt
+      i = i+1
+    }
+    //1. 입력 값 input 을 Vector 로 변경
+    solution(inputMutableArray.toVector)
   }
+
   def solution(answers: Vector[Int]): Vector[Int] = {
 
+    val student1 = List(1, 2, 3, 4, 5)
+    val student2 = List(2, 1, 2, 3, 2, 4, 2, 5)
+    val student3 = List(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
 
-    println(">> answers: ",answers)
+    val scoreList:MutableList[Int] = MutableList(0,0,0)
 
-    return Vector[Int]()
 
-  }
+    for ( i <- 0 until answers.length) {
+      if (answers(i) == student1(i%5) ) {
+        scoreList(0) += 1
+      }
+      if (answers(i) == student2(i%8) ) {
+        scoreList(1) += 1
+      }
+      if (answers(i) == student3(i%10) ) {
+        scoreList(2) += 1
+      }
+    }
 
-  def lengthCheck(answers: Vector[Int]): Int ={
-    return answers.length
+    val maxScore = scoreList.max
+    var returnLength = 0
+    scoreList.foreach( x => if (x == maxScore) {
+      returnLength += 1
+    })
+    var sortStudentList = new Array[Int](returnLength)
+
+    var j = 0
+    for ( i <- 0 until scoreList.length) {
+      if (scoreList(i) == maxScore) {
+        sortStudentList(j) = i+1
+        j += 1
+      }
+    }
+
+    return sortStudentList.toVector
+
   }
 }
