3ed32213b682546802dca2c426451d5c95c52b0a
이주연 <juyeonlee@tmon.co.kr>
2019-12-24 18:13:19 +0900

    서울에서 김서방 찾기

---
+++ src/main/scala/Level_1/cresencia/findKimInSeoul.scala
@@ -0,0 +1,33 @@
+package Level_1.cresencia
+
+/**
+문제 설명
+String형 배열 seoul의 element중 Kim의 위치 x를 찾아, 김서방은 x에 있다는 String을 반환하는 함수, solution을 완성하세요. seoul에 Kim은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
+
+제한 사항
+seoul은 길이 1 이상, 1000 이하인 배열입니다.
+seoul의 원소는 길이 1 이상, 20 이하인 문자열입니다.
+Kim은 반드시 seoul 안에 포함되어 있습니다.
+입출력 예
+seoul	return
+[Jane, Kim]	김서방은 1에 있다
+  */
+object findKimInSeoul {
+
+  def main(args: Array[String]): Unit = {
+
+    val seoul:Vector[String] = Vector("Jane","Kim")
+
+    println(solution(seoul))
+  }
+
+  def solution(seoul: Vector[String]): String = {
+    var where = ""
+
+    for(idx <- 0 until seoul.length){
+      if(seoul(idx).contains("Kim")) where = idx.toString
+    }
+
+    return "김서방은 "+where+"에 있다"
+  }
+}
