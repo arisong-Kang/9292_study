cbf73169b7a40b98b019b088ad538b6022618899
한용준 <dydwnsekd123@gmail.com>
2020-01-05 22:05:24 +0900

    이상한 문자 만들기

---
+++ src/main/scala/Level_1/dydwnsekd/Level1_16.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = ''
+    split_s = s.split(" ")
+    
+    for ss in split_s:
+        for i in range(len(ss)):
+            if i % 2 == 0:
+                answer += ss[i].upper()
+            else:
+                answer += ss[i].lower()
+        
+        answer += " "
+    
+    return answer[:-1]
