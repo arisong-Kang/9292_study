a36f95a52dbcfa52c3bf137485f5412a98343c77
한용준 <dydwnsekd123@gmail.com>
2020-01-08 23:48:04 +0900

    정수 제곱근 판별

---
+++ src/main/scala/Level_1/dydwnsekd/Level1_19.py
@@ -0,0 +1,12 @@
+import math
+
+def solution(n):
+    answer = 0
+    
+    sqrt_n = math.sqrt(n)
+    if int(sqrt_n) == sqrt_n:
+        answer = (int(sqrt_n)+1) ** 2
+    else:
+        answer = -1
+        
+    return answer
