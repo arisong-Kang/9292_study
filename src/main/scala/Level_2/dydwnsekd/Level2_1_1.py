0624338012cdbc96dd55a3ea7e503b1f8dc8cc93
한용준 <dydwnsekd123@gmail.com>
2020-01-21 22:08:03 +0900

    쇠막대기 완료

--- src/main/scala/Level_2/dydwnsekd/Level2_1_1.py
+++ src/main/scala/Level_2/dydwnsekd/Level2_1_1.py
@@ -2,15 +2,13 @@ def solution(arrangement):
     answer = 0
     pipe = 0
     
-    for i in range(len(arrangement)-1):
+    for i in range(len(arrangement)):
         if arrangement[i] == '(' and arrangement[i+1] == ')':
-            print (pipe)
             answer += pipe
         elif arrangement[i] == '(':
             pipe += 1
-            print (pipe)
         elif arrangement[i] == ')' and arrangement[i-1] != '(':
             pipe -= 1
-            print (pipe)
-        
+            answer += 1
+            
     return answer
