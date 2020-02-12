29e19a5d2b3ebadea841dec42d44cf1e549cec8e
이주연 <juyeonlee@tmon.co.kr>
2020-02-12 14:36:55 +0900

    트럭, 탑, 프린터

--- src/main/scala/Level_2/cresencia/printer_2.scala
+++ src/main/scala/Level_2/cresencia/printer_2.scala
@@ -34,7 +34,7 @@ priorities	location	return
 6개의 문서(A, B, C, D, E, F)가 인쇄 대기목록에 있고 중요도가 1 1 9 1 1 1 이므로 C D E F A B 순으로 인쇄합니다.
 
   */
-object printer_2 {
+object Printer_2 {
 
   def main(args:Array[String]):Unit = {
     val priorities = Vector(2,1,3,2)
