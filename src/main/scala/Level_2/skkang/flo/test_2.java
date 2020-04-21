4b91f7c16a5859009ce7ca090166aefbc0062047
skkang <seolgi911@gmail.com>
2020-04-21 19:16:46 +0900

    test_2

---
+++ src/main/scala/Level_2/skkang/flo/test_2.java
@@ -0,0 +1,152 @@
+package Level_2.skkang.flo;
+
+import java.util.ArrayList;
+
+/**
+ * Created by skkang on 2020. 4. 20..
+ */
+public class test_2 {
+
+    public static void main(String[] args) {
+
+//        int N = 10;
+//        int K = 3;
+//        int W = 10;
+
+//        int N = 10;
+//        int K = 1;
+//        int W = 10;
+
+//        int N = 6;
+//        int K = 1;
+//        int W = 10;
+
+        int N = 21;
+        int K = 17;
+        int W = 10;
+
+        solution(N, K, W);
+
+    }
+
+    public static ArrayList<ArrayList<Integer>> stopArray = new ArrayList<>();
+
+    public static double solution(int N, int K, int W) {
+
+        System.out.println("카드 수: " + W + ", 언제까지 뽑을까?(K<=합) : " + K + ", 승패?(N>=합):" + N + ", ");
+
+        ArrayList<Double> percentList = new ArrayList<>();
+
+        for (int i = 1 ; i <= W ; i++) {
+            System.out.println();
+            System.out.println(">>>>>>> i: " + i);
+            // 전체 경우의 수
+            int totalCount = comb(W,i);
+            System.out.println("전체 경우의 수: " + comb(W,i));
+
+            // 카드를 뽑은 경우의 수(K<=합 일때 STOP)
+            comb(new int[i], 0, W, i, 0, K);
+            System.out.println("stopArray.size(): " + stopArray.size());
+
+            if(stopArray.size() > 0) {
+                // 뽑은 카드의 경우의 수 중 이겼을 때 수
+                win(stopArray, N);
+                System.out.println("WIN count: " + win(stopArray, N));
+
+                for( ArrayList<Integer> tmp : stopArray ){
+                    for( int n : tmp ) {
+                        System.out.print( n + " ");
+                    }
+                    System.out.print(", ");
+
+                }
+            }
+
+            System.out.println();
+            System.out.println("percent: " + (double)win(stopArray, N)/(double)totalCount);
+
+            Double percent = (double)win(stopArray, N)/(double)totalCount;
+            if( 0 < percent ) {
+                percentList.add(percent);
+            }
+
+            if (totalCount == stopArray.size()) {
+                break;
+            }
+
+            stopArray.clear();
+        }
+
+
+        if (percentList.size() > 0) {
+
+            double result = percentList.get(0);
+            for (int j = 1 ; j < percentList.size() ; j++) {
+                System.out.println(percentList.get(j));
+                result = result + percentList.get(j);
+            }
+            System.out.println("result: " + Math.round(result*100)/100.0);
+        }
+
+        return 0;
+    }
+
+    static int win(ArrayList<ArrayList<Integer>> stopArray, int N) {
+        int winCount = 0;
+        for( ArrayList<Integer> tmp : stopArray ){
+            int sum = 0;
+
+            for( int n : tmp ) {
+//                System.out.print( n + " ");
+                sum += n;
+            }
+
+            if ( N >= sum ) {
+                winCount++;
+            }
+//            System.out.println("sum = " + sum);
+        }
+
+        return winCount;
+    }
+
+    //조합의 각각의 경우
+    static void comb(int[] arr, int index, int W, int r, int target, int K) {
+
+        ArrayList<Integer> tmp = new ArrayList<>();
+
+        int sum = 0;
+
+        if ( r == 0) {
+            if (arr[0] >=1 ) {
+                for(int i = 0 ; i < arr.length ; i++) {
+//                    System.out.print(arr[i] + " ");
+                    tmp.add(arr[i]);
+                    sum += arr[i];
+                }
+
+                if ( K <= sum ) {
+                    stopArray.add(tmp);
+                }
+            }
+
+        } else if(target == W + 1 ){
+            return;
+        } else {
+            arr[index] = target;
+            comb(arr, index+1, W, r-1, target+1, K);
+            comb(arr, index, W, r, target+1, K);
+        }
+    }
+
+    //조합의 갯수
+    static int comb(int W, int r) {
+        if ( r == 0 || W == r) {
+            return 1;
+        } else {
+            return comb(W-1, r -1) + comb(W-1, r);
+        }
+    }
+
+
+}
